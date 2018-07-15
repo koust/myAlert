f1b83f1c392f035e301408c5e05c446c3a51cc8e
Batuhan Saygılı <batuhansaygili94@gmail.com>
2018-07-15 17:55:25 +0300

    Initial Commit

---
+++ myAlert/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myAlert
+//
+//  Created by Batuhan Saygili on 15.07.2018.
+//  Copyright © 2018 Batuhan Saygili. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
